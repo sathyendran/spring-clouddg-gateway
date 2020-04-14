3e36118e8b22f195c522f7bf7c1cf78736dd3f50
Sathyendran <37661333+sathinarayanan@users.noreply.github.com>
2020-04-14 15:18:19 +0530

    Initial commit

---
+++ src/main/java/org/sathya/springclouddggateway/SpringClouddgGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.sathya.springclouddggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClouddgGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClouddgGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sathya/springclouddggateway/SpringClouddgGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sathya.springclouddggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClouddgGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
